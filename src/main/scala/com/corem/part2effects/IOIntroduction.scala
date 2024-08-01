edf65b17906aa201e8b03b17ca5f66270044d4ef
core <cornet.remi@gmail.com>
2024-08-01 19:58:25 -0400

    IOIntroduction

--- src/main/scala/com/corem/part2effects/IOIntroduction.scala
+++ src/main/scala/com/corem/part2effects/IOIntroduction.scala
@@ -41,11 +41,83 @@ object IOIntroduction:
 
   /*
     Exercices:
-
+    - 1. Sequence two IOs and take the result of the last one
+    - 2. Sequence two IOs and take the result of the first one
+    - 3. Repeat an IO forever
+    - 4. Convert an IO to a different type
+    - 5. Discard value inside an IO, just return Unit
+    - 6. Fix stack recursion
+    - 7. Write a fibonacci IO that does not crash on recursion
    */
+
+  def sequenceTakeLast[A,B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa.flatMap(_ => iob)
+
+  def sequenceTakeLastV2[A,B](ioa: IO[A], iob: IO[B]): IO[B] =
+    ioa *> iob
+
+  def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa.flatMap(a =>iob.map(_ => a))
+
+  def sequenceTakeFirstV2[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+    ioa <* iob
+
+  def forever[A](io: IO[A]): IO[A] =
+    io.flatMap(_ => forever(io))
+
+  def foreverV2[A](io: IO[A]): IO[A] =
+    io >> foreverV2(io)
+
+  def foreverV3[A](io: IO[A]): IO[A] =
+    io *> foreverV3(io)
+
+  def foreverV4[A](io: IO[A]): IO[A] =
+    io.foreverM
+
+  def convert[A, B](io: IO[A], value: B): IO[B] =
+    io.map(_ => value)
+
+  def convertV2[A, B](io: IO[A], value: B): IO[B] =
+    io.as(value)
+
+  def asUnit[A](io: IO[A]): IO[Unit] =
+    io.map(_ => ())
+
+  def asUnitV2[A](io: IO[A]): IO[Unit] =
+    io.as(()) // Not readable
+
+  def asUnitV3[A](io: IO[A]): IO[Unit] =
+    io.void
+
+  def sum(n: Int): Int =
+    if n <= 0 then 0
+    else n + sum(n - 1)
+
+  def sumIO(n: Int): IO[Int] =
+    if n <= 0 then IO(0)
+    else for
+      lastNumber <- IO(n)
+      prevSum <- sumIO(n - 1)
+    yield prevSum + lastNumber
+
+  def fibonacci(n: Int): IO[BigInt] =
+    if n < 2 then return IO(1)
+    else for
+      last <- IO.defer(fibonacci(n - 1))
+      prev <- IO.defer(fibonacci(n - 2))
+    yield last + prev
 
   def main(args: Array[String]): Unit =
     import cats.effect.unsafe.implicits.global
 //    println(aDelayedIO.unsafeRunSync())
 //    println(smallProgram.unsafeRunSync())
-    println(smallProgramV2.unsafeRunSync())
+//    println(smallProgramV2.unsafeRunSync())
+
+//    foreverV3(IO {
+//      Thread.sleep(1000)
+//      println("Forever!")
+//    }).unsafeRunSync()
+
+//    println(sumIO(20000).unsafeRunSync())
+//    sum(20000)
+    (1 to 100).foreach(i => println(fibonacci(i).unsafeRunSync()))
